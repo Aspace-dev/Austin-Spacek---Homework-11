ae674456369e4575a20d6a440ea34a065a5f7d91
PELAGIVS <54682204+Aspace-dev@users.noreply.github.com>
2020-05-07 23:00:49 -0500

    Rewrote Chart and Graph code. New code renders all data properly.

--- static/js/app.js
+++ static/js/app.js
@@ -1,84 +1,85 @@
-// @TODO: Complete the following function that builds the metadata panel
+// Building metadata panel
 function buildMetadata(sample) {
-  // Use `d3.json` to fetch the metadata for a sample
-  const select_smd = d3.select("#sample-metadata")
-  d3.json(`/metadata/${sample}`).then((data) => {
-    select_smd
-      .html(""); // Use `.html("") to clear any existing metadata
-    // Use `Object.entries` to add each key and value pair to the panel
+  // Fetching the metadata with d3.json
+  d3.json("/metadata/" + sample).then(data => {
+    // Selecting panel for sample data
+    const BBmetadata = d3.select("#sample-metadata");
+    // Clearing previous data
+    BBmetadata
+      .html("");
+    // Appending key value pairs (each with tag) to panel.
     Object.entries(data).forEach(([key, value]) => {
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
-      select_smd
-        .append("p")
-        .text(`${key} ${value}`)
-        .property("value", sample);
+      BBmetadata
+        .append("panel")
+        .html(key + " : " + value + "<br>");
     });
   });
 }
 
     // Didn't attempt the bonus!
-    // I feel like this is the correct code and I am missing one more piece in the map section
-    // The data isn't getting rendered. I think it is an issue with passing the correct thing.
-    // Please let me know how I can correct this issue in the comment section! 
-    
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 
-
-// @TODO: Build a Bubble Chart and Pie Chart
+// Building Pie Graph and Bubble Chart
 function buildCharts(sample) {
-    // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.json(`/samples/${sample}`).then((plotSample) => {
-    const select_bubble = d3.select("#bubble")
-    const select_pie = d3.select("#pie")
-    select_bubble
-      .html("");
-    select_pie
-      .html("");
-    Object.entries(plotSample).forEach((plotSampledata) => {
-      var trace1 = {
-        x: plotSampledata.map( e => e.otu_ids),
-        y: plotSampledata.map( e => e.sample_values),
-        text: [plotSampledata.map( e => e.otu_labels)],
-        mode: 'markers',
+  // Fetching the sample data with d3.json
+  d3.json("/samples/" + sample).then(data => {
+    // Pie Graph
+    // Slicing the top ten samples from the sample data
+    const graphHt = data.otu_labels.slice(0, 10);
+    const graphLabels = data.otu_ids.slice(0, 10);
+    const graphValues = data.sample_values.slice(0, 10);
+
+    const graphData = [
+      {
+        values: graphValues,
+        labels: graphLabels,
+        hovertext: graphHt,
+        type: "pie"
+      }
+    ];
+
+    const graphLayout = {
+      height: 400,
+      width: 500,
+      title: "Top Ten Samples"
+    };
+
+    Plotly.newPlot("pie", graphData, graphLayout);
+    
+    // Bubble Chart
+    const chartData = [
+      {
+        x: data.otu_ids,
+        y: data.sample_values,
+        text: data.otu_labels,
+        mode: "markers",
         marker: {
-          color: [plotSampledata.map( e => e.otu_ids)],
-          size: plotSampledata.map( e => e.sample_values)
+          color: data.otu_ids,
+          size: data.sample_values,
+          colorscale: "Bluered" // Cool "Bluered" color scale I found on Plotly docs
         }
-      };
-        
-      var plotSampledata = [trace1];
-        
-      var layout = {
-        title: 'Belly Button Sample Bubble Chart',
-        showlegend: false,
-        height: 600,
-        width: 600
-      };
-        
-      Plotly.newPlot('bubble', plotSampledata, layout);
-      
+      }
+    ];
 
-      // @TODO: Build a Pie Chart
-      // HINT: You will need to use slice() to grab the top 10 sample_values,
-      // otu_ids, and labels (10 each).
-      var trace2 = {
-        values: plotSampledata.map( e => e.sample_values).slice(0,10),
-        labels: plotSampledata.map( e => e.otu_ids).slice(0,10),
-        hoverinfo: plotSampledata.map( e => e.otu_labels).slice(0,10),
-        type: 'pie'
-      };
-      
-      var plotSampledata2 = [trace2];
+    const chartLayout = {
+      title: "Samples for each OUT ID",
+      showlegend: false,
+      height: 600,
+      width: 1200,
+      xaxis: {
+        title: {
+          text: "OTU ID"
+        }
+      },
+      yaxis: {
+        title: {
+          text: "Count of Samples"
+        }
+      }
+    };
 
-      var layout2 = {
-        height: 400,
-        width: 500
-      };
-      
-      Plotly.newPlot('pie', plotSampledata2, layout2);
-    });  
+    Plotly.newPlot("bubble", chartData, chartLayout);
   });
 }
 
